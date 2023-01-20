647fd8fe67b5b6cc0754fe5958cc2cf6bbd2cb3e
Elena <bespalova.e.v@mail.ru>
2023-01-20 12:35:40 +0800

    task01HW

--- Seminar5/HW5/task01.py
+++ Seminar5/HW5/task01.py
@@ -4,3 +4,29 @@
 # out
 # авб абв бав абв вба бав вба абв абв абв
 # авб бав вба бав вба
+
+import random
+
+
+
+def make_list():
+    num = int(input("Input number of words "))
+    word = input("Input letter :\n")
+    my_list = []
+    for i in range(num):
+        text = random.sample(word, 3)
+        my_list.append("".join(text))
+    print(my_list)
+    return my_list
+
+def search_letter (my_list):
+    find_txt = input("Input 3 Letters  ")
+    new_list = []
+    for i in range (len(my_list)):
+        temp = my_list[i]
+        if find_txt not in my_list[i]:
+           new_list.append(temp)
+        
+    print(new_list)
+  
+search_letter(make_list())
