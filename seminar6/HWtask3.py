c66c210990abad59d1ac4c3375b13afa94763789
Elena <bespalova.e.v@mail.ru>
2023-02-06 21:48:19 +0700

    hw

--- seminar6/HWtask3.py
+++ seminar6/HWtask3.py
@@ -6,19 +6,59 @@
 
 # {'А': ['Алина'], 'Б': ['Бибочка'], 'И': ['Иван', 'Илья'], 'М': ['Марина', 'Мария'], 'П': ['Петр', 'Петр']}
 
-def make_list ():
-    make_list = [str(i) for i in input("Input names ").split()]
-    print (make_list)
-    return make_list
+# def make_list ():
+#     make_list = [str(i) for i in input("Input names ").split()]
+#     print (make_list)
+#     return make_list
 
-def dictionary_new (make_list):
-    make_dict = {}
-    make_list.sort()
-    for name in make_list:
-        key = name[0]
-        if key not in make_dict:
-            make_dict[key] = []
-        make_dict[key].append(name)
-    print (make_dict)
+# def dictionary_new (make_list):
+#     make_dict = {}
+#     make_list.sort()
+#     for name in make_list:
+#         key = name[0]
+#         if key not in make_dict:
+#             make_dict[key] = []
+#         make_dict[key].append(name)
+#     print (make_dict)
     
-dictionary_new(make_list())
+# dictionary_new(make_list())
+
+
+def thesaurus(*args):
+    names_dict = {}
+    for i in sorted(args):
+        letter = i[0]
+        if letter not in names_dict:
+            names_dict[letter] = [i]
+        names_dict[letter] += [i]
+
+    return names_dict
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка"))
+
+#  ________________________________________________________ 2 вариант
+from itertools import groupby
+
+
+def thesaurus(*args):
+    if "" not in args:
+        return {ch: list(names) for ch, names in groupby(sorted(args), key=lambda i: i[0]) if ch}
+    return "Error"
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Петр", "Алина", "Бибочка"))
+
+
+#  ________________________________________________________ 3 вариант
+
+def thesaurus(*args):
+    if "" not in args:
+        return {ch[0]: list(filter(lambda el: el.startswith(ch[0]), args)) for ch in sorted(args)}
+    return "Error"
+
+
+thesaurus('Кармен', 'Андрей', 'Василий', 'Алексей', 'Дмитрий', 'Виктор', 'Инна', 'Александра', 'Игнат', 'Спартак',
+          'Якоб', 'Люция', 'Дионис', 'Агора', 'Игорь')
+
+
