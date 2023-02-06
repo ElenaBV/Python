ba7d4b25ee0b28ea96426994f043b3416343fbb2
Elena <bespalova.e.v@mail.ru>
2023-02-06 16:51:32 +0700

    HW^

--- seminar6/HWtask3.py
+++ seminar6/HWtask3.py
@@ -5,3 +5,20 @@
 # out
 
 # {'А': ['Алина'], 'Б': ['Бибочка'], 'И': ['Иван', 'Илья'], 'М': ['Марина', 'Мария'], 'П': ['Петр', 'Петр']}
+
+def make_list ():
+    make_list = [str(i) for i in input("Input names ").split()]
+    print (make_list)
+    return make_list
+
+def dictionary_new (make_list):
+    make_dict = {}
+    make_list.sort()
+    for name in make_list:
+        key = name[0]
+        if key not in make_dict:
+            make_dict[key] = []
+        make_dict[key].append(name)
+    print (make_dict)
+    
+dictionary_new(make_list())
